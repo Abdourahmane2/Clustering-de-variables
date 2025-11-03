2b03d78837cb6fa9d92b5397acd8d7ac5701a5ab
Abdourahmane2 <105498163+Abdourahmane2@users.noreply.github.com>
2025-11-04 00:56:00 +0100

    commit

--- app/server.R
+++ app/server.R
@@ -1,26 +1,81 @@
-server <- function(input, output) {
+library(DT)
 
+server <- function(input, output, session) {
   data <- reactive({
     req(input$fichier)
-    ext <- tools::file_ext(input$fichier$datapath)
-    switch(ext,
-           csv = read.csv(input$fichier$datapath, header = TRUE, sep = input$separateur),
-           xlsx = read_excel(input$fichier$datapath),
-           xls = read_excel(input$fichier$datapath),
-           validate("Format de fichier non pris en charge")
+    ext <- tools::file_ext(input$fichier$name)
+
+    df <- tryCatch({
+      if (ext == "csv") {
+        read.csv(input$fichier$datapath, sep = input$separateur)
+      } else if (ext %in% c("xlsx", "xls")) {
+        read_excel(input$fichier$datapath)
+      } else {
+        stop("Format non pris en charge.")
+      }
+    }, error = function(e) {
+      showNotification(paste("Erreur :", e$message), type = "error")
+      return(NULL)
+    })
+    df
+  })
+
+  observeEvent(input$valider, {
+    req(data())
+    showNotification("✅ Importation réussie !", type = "message")
+    updateNavbarPage(session, "onglets", selected = "Nettoyage")
+  })
+
+  # --- Tableau page Importation ---
+  output$tableau_import <- renderDT({
+    req(data())
+    datatable(
+      head(data(), 10),
+      options = list(pageLength = 10, scrollX = TRUE),
+      rownames = FALSE
     )
   })
 
-  # Table pour apercu des données
-  output$tableau <- renderTable({
+
+  # --- Tableau page Clustering ---
+  output$tableau_cluster <- renderTable({
     req(data())
     head(data(), 10)
   })
-
-  # Variable pour rendre le conditionalPanel visible
-  output$data_loaded <- reactive({
+  #-- t ableau netoyage ---
+  output$tableau_importe_nettoye <- renderDT({
     req(data())
-    TRUE
+    datatable(
+      head(data(), 10),
+      options = list(pageLength = 10, scrollX = TRUE),
+      rownames = FALSE
+    )
   })
-  outputOptions(output, "data_loaded", suspendWhenHidden = FALSE)
+
+  #====affichage des stat =====
+  output$statistiques_ui <- renderUI({
+    req(data)
+    df <- data()
+
+    tagList(
+      h4("📊 Statistiques descriptives"),
+      datatable(summary(df)),
+
+      h4("📐 Dimensions"),
+      HTML(paste("Nombre de lignes :", nrow(df), "<br>",
+                 "Nombre de colonnes :", ncol(df))),
+
+      h4("📝 Types des colonnes"),
+      #affichage du type  pour chaque colonne
+      datatable(data.frame(Colonne = names(df), Type = sapply(df, class)) ,
+                options = list(pageLength = 10, scrollX = TRUE),
+                choices = list(searching = FALSE, lengthChange = FALSE),
+                rownames = FALSE) ,
+      #changer le type de chaque colonne si on veut
+
+
+    )
+  })
+
+
 }
--- app/ui.R
+++ app/ui.R
@@ -1,26 +1,73 @@
 library(shiny)
 library(readxl)
+library(DT)
 
-ui <- fluidPage(
-  titlePanel("Importation CSV/Excel"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput(
-        "fichier", "Importer un fichier CSV ou Excel",
-        accept = c(".csv", ".xlsx", ".xls")
-      ),
-      selectInput("separateur", "Séparateur", choices = c(Virgule = ",", Point_Virgule = ";", Tabulation = "\t")),
-      conditionalPanel(
-        condition = "output.data_loaded == true",
-        selectInput("method", "Méthode de clustering", choices = c("kmeans", "kmodes", "hierarchical")),
-        numericInput("k", "Nombre de clusters", value = 2, min = 2),
-        actionButton("lancer", "Lancer le clustering")
-      )
-    ),
-    mainPanel(
-      tableOutput("tableau"),
-      tableOutput("cluster"),
-      verbatimTextOutput("summary")
-    )
+ui <- navbarPage(
+  id = "onglets",  # nécessaire pour updateNavbarPage
+  title = "Analyse de clustering",
+
+  # ================================= page d'importation de fichier ===========================
+  tabPanel("Importation",
+           sidebarLayout(
+             sidebarPanel(
+               fileInput(
+                 "fichier", "Importer un fichier CSV ou Excel",
+                 accept = c(".csv", ".xlsx", ".xls")
+               ),
+               selectInput(
+                 "separateur", "Séparateur",
+                 choices = c(Virgule = ",",
+                             `Point-virgule` = ";",
+                             Tabulation = "\t")
+               ),
+               actionButton("valider", "Valider l'importation")
+             ),
+             mainPanel(
+               h4("Aperçu des données importées"),
+               DTOutput("tableau_import")
+             )
+           )
+  ),
+
+  #=========================page de nettoyage =============================
+  tabPanel("Nettoyage",
+           sidebarLayout(
+             sidebarPanel(
+              checkboxInput("supprimer_na", "Supprimer les lignes avec des valeurs manquantes", value = FALSE),
+              checkboxInput("normaliser", "normaliser les donnes numeriques ", value = FALSE),
+              checkboxInput("supprimer_outliers", "Supprimer les valeurs aberantes ", value = FALSE),
+              actionButton("nettoyer", "appliquer le nettoyage") ,
+
+             ),
+             mainPanel(
+               h4("tableau nettoye") ,
+               tabsetPanel(
+                 tabPanel("Aperçu du tableau", DTOutput("tableau_importe_nettoye")),
+                 tabPanel("Statistiques", uiOutput("statistiques_ui"))
+               )
+             )
+           )
+  ),
+
+  # ================================page de clustering  ==================================
+
+  tabPanel("Clustering",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput("method", "Méthode de clustering",
+                           choices = c("kmeans", "hierarchical")),
+               numericInput("k", "Nombre de clusters", value = 2, min = 2),
+               actionButton("lancer", "Lancer le clustering"),
+
+             ),
+             mainPanel(
+               h4("Aperçu des données importées"),
+               tableOutput("tableau_cluster"),
+               hr(),
+               h4("Résultats du clustering"),
+               tableOutput("cluster"),
+               verbatimTextOutput("summary")
+             )
+           )
   )
 )
